ce46945a6fd2a6ecc3a96a5b0027e1fee0d0ab57
Patryk Gneba <gnebapatryk@gmail.com>
2022-07-13 00:17:43 +0200

    Attribiut for dependency injection of services

---
+++ Utils/Attribiutes/InjectableAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace Utils.Attribiutes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class InjectableAttribute : Attribute
+    {
+        public Type ImplementedInterface { get; }
+        public DependencyInjectionScope Scope { get; }
+
+        public InjectableAttribute(Type implementedInterface, DependencyInjectionScope scope = DependencyInjectionScope.Singleton)
+        {
+            ImplementedInterface = implementedInterface;
+            Scope = scope;
+        }
+    }
+
+    public enum DependencyInjectionScope
+    {
+        Singleton,
+        PerDependency,
+        Scoped
+    }
+}
