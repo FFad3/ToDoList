882c86c8df5c6a681c92a9a29360aae7cf260934
Patryk Gneba <gnebapatryk@gmail.com>
2022-07-12 20:27:49 +0200

    Domain Entities

---
+++ Core/Enttities/Priority.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Enttities
+{
+    public class Priority
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string PriorytyLevel { get; set; }
+    }
+}
---
+++ Core/Enttities/ToDoTask.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Enttities
+{
+    public class ToDoTask
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(1), MaxLength(100)]
+        public string TaskName { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+
+        //Priorytet taska np.High/Low/Medium
+        [Required]
+        public int PriorytyId { get; set; }
+
+        public Priority Priority { get; set; }
+    }
+}
